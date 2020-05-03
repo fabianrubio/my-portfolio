5715dde213b7a609e7903acbdd18db8f1d6d9e95
Fabian <frubio@lumbrera.com.mx>
2020-05-02 23:18:42 -0500

    feat: new features

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import { FaTwitter, FaGithub, FaMedium } from 'react-icons/fa'
-import { StaticQuery, graphql } from 'gatsby'
-import './style.scss'
-import Emoji from './emoji'
+import React from "react";
+import { FaTwitter, FaGithub, FaMedium } from "react-icons/fa";
+import { StaticQuery, graphql } from "gatsby";
+import "./style.scss";
+import Emoji from "./emoji";
 
 const Footer = () => (
   <StaticQuery
@@ -19,48 +19,12 @@ const Footer = () => (
         }
       }
     `}
-    render={data => (
-      <footer className='footer center has-background-light'>
-        <div className='content has-text-centered'>
-          <p className='is-size-4'>
-            This website was handcrafted with plenty cups of{' '}
-            <Emoji emoji='☕' />
-          </p>
-          <p className='is-size-4'>
-            By Aman Mittal (@amanhimself) using{' '}
-            <a href={data.site.siteMetadata.gatsby}>Gatsby</a> +{' '}
-            <a href={data.site.siteMetadata.bulma}>Bulma</a>
-          </p>
-          <article className='media center'>
-            <span className='icon'>
-              <a href={data.site.siteMetadata.twitter}>
-                <FaTwitter size='fa-2x' color='blue' />
-              </a>
-            </span>
-            &nbsp;
-            <span className='icon'>
-              <a href={data.site.siteMetadata.github}>
-                <FaGithub size='fa-2x' color='black' />
-              </a>
-            </span>
-            &nbsp;
-            <span className='icon'>
-              <a href={data.site.siteMetadata.medium}>
-                <FaMedium size='fa-2x' color='green' />
-              </a>
-            </span>
-            &nbsp;
-          </article>
-          &nbsp;
-          <p className='is-size-5'>
-            You can also back or support this project for me to keep it updated
-            by{' '}
-            <a href='https://www.paypal.me/amanhimself/2'>Buying Me a Coffee</a>
-          </p>
-        </div>
+    render={(data) => (
+      <footer className="footer center has-background-light">
+        <div className="content has-text-centered"></div>
       </footer>
     )}
   />
-)
+);
 
-export default Footer
+export default Footer;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,41 +1,42 @@
-import React from 'react';
-import { FaGithub } from 'react-icons/fa';
+import React from "react";
+import { FaGithub } from "react-icons/fa";
 
-import './style.scss';
-import gatsbyLogo from '../images/gatsby-icon.png';
-import bulmaLogo from '../images/bulma-logo.png';
+import "./style.scss";
+import gatsbyLogo from "../images/gatsby-icon.png";
+import bulmaLogo from "../images/bulma-logo.png";
 
 const Navbar = () => (
-	<div className="hero-head is-hidden-mobile">
-		<nav className="navbar">
-			<div className="container">
-				<div className="navbar-brand">
-					<a
-						className="navbar-item"
-						href="https://github.com/amandeepmittal/gatsby-bulma-quickstart"
-					>
-						<img src={gatsbyLogo} alt="Logo-1" />
-						<img src={bulmaLogo} alt="Logo-2" />
-					</a>
-				</div>
-				<div id="navbarMenuHeroA" className="navbar-menu">
-					<div className="navbar-end">
-						<span className="navbar-item">
-							<a
-								className="button is-danger is-inverted"
-								href="https://github.com/amandeepmittal/gatsby-bulma-quickstart"
-							>
-								<span className="icon">
-									<FaGithub size="fa-2x" />
-								</span>
-								<span>Download</span>
-							</a>
-						</span>
-					</div>
-				</div>
-			</div>
-		</nav>
-	</div>
+  <div className="hero-head is-hidden-mobile">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="#home">
+          FR
+        </a>
+
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-end">
+          <a class="navbar-item">Home</a>
+          <a class="navbar-item">About</a>
+          <a class="navbar-item">Skills</a>
+          <a class="navbar-item">Experience</a>
+          <a class="navbar-item">Contact</a>
+        </div>
+      </div>
+    </nav>
+  </div>
 );
 
 export default Navbar;
